ba975dc10e23f3bfbf0fdef6e1eed34e985b2172
Diyorbek Mamataliyev <91861166+DiyorbekMamataliyev@users.noreply.github.com>
2024-05-15 21:50:24 +0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+
+# create ad flask instance
+app = Flask(__name__)
+
+
+# create a route decorator
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+
+@app.route('/elonlar')
+def elon():
+    return render_template("elon.html")
+
+
+@app.route('/yonalishlar')
+def yonalish():
+    return render_template("yonalishlar.html")
+
+
+@app.route('/jadval')
+def jadval():
+    return render_template("jadval.html")
+
+
+@app.route('/nazoratlar')
+def nazorat():
+    return render_template("nazoratlar.html")
+
+
+@app.route('/yakuniy')
+def yakuniy():
+    return render_template("yakuniy.html")
+
+
+@app.route('/murojaat')
+def murojaat():
+    return render_template("murojaat.html")
+
+
+# localhost:5000/user/john
+@app.route('/user/<name>')
+def user(name):
+    return f"<h1>Hello, {name}</h1>"
